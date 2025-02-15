4892ae73f545514096f15fa9f61b8768fd1bd7ca
Yaroslav Bolyukin <iam@lach.pw>
2025-02-15 17:47:23 +0100

    python3Packages.qubes-core-qubesdb: init at 4.2.7

--- pkgs/top-level/python-overlay.nix
+++ pkgs/top-level/python-overlay.nix
@@ -8,6 +8,12 @@ let
     ;
 in
 {
+  qubes-core-qubesdb =
+    (pkgs.qubes-core-qubesdb.override {
+      inherit python;
+      withPython = true;
+    }).pythonModule;
+
   qubes-vmm-xen = toPythonModule (
     pkgs.qubes-vmm-xen.override {
       python3Packages = self;
