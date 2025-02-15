69b232e30201ac0b8b91a7eea5d66b9a8e01c1af
Yaroslav Bolyukin <iam@lach.pw>
2025-02-15 18:02:34 +0100

    qemu_qubes: init at 9.2.0

--- pkgs/top-level/overlay.nix
+++ pkgs/top-level/overlay.nix
@@ -1,4 +1,11 @@
 self: super: {
   python3 = super.python3.override { packageOverrides = import ./python-overlay.nix; };
 
+  qemu_qubes = self.lib.lowPrio (
+    self.qemu.override {
+      hostCpuTargets = [ "i386-softmmu" ];
+      xenSupport = true;
+      xen = self.qubes-vmm-xen;
+    }
+  );
 }
