a84e8b39e0d63a9bede1f4ecf8648a5721186111
Yaroslav Bolyukin <iam@lach.pw>
2026-04-18 23:48:37 +0200

    qubes-vmm-xen: add replaceVars for 0002-scripts patch (slop)
    Prompt: xenconsoled.service has @grep@ unsubstituted - 0002-scripts patch needs replaceVars
    
    Co-Authored-With: Claude Opus 4.6 (1M context)

--- pkgs/by-name/qu/qubes-vmm-xen/package.nix
+++ pkgs/by-name/qu/qubes-vmm-xen/package.nix
@@ -6,10 +6,71 @@
   qubes-seabios,
   qubes-vmm-stubdom-linux,
   python3Packages,
+  replaceVars,
+  # Dependencies for 0002-scripts-external-executable-calls.patch
+  bridge-utils,
+  coreutils,
+  diffutils,
+  drbd,
+  gawk,
+  gnugrep,
+  gnused,
+  inetutils,
+  iproute2,
+  iptables,
+  kmod,
+  libnl,
+  multipath-tools,
+  nbd,
+  openiscsi,
+  openvswitch,
+  psmisc,
+  systemd,
+  util-linux,
+  which,
 }:
 let
+  inherit (lib) genAttrs getExe getExe';
+
   version = "4.19.4";
   revision = "7";
+
+  # Substitutions for 0002-scripts-external-executable-calls.patch
+  # Mirrors scriptDeps from nixpkgs xen package
+  scriptDeps =
+    let
+      mkTools = pkg: tools: genAttrs tools (tool: getExe' pkg tool);
+    in
+    (genAttrs [
+      "CONFIG_DIR" "CONFIG_LEAF_DIR" "LIBEXEC_BIN"
+      "XEN_LOG_DIR" "XEN_RUN_DIR" "XEN_SCRIPT_DIR"
+      "qemu_xen_systemd" "sbindir"
+    ] (_: null))
+    // (mkTools coreutils [
+      "basename" "cat" "cp" "cut" "dirname" "head"
+      "ls" "mkdir" "mktemp" "readlink" "rm" "seq" "sleep" "stat"
+    ])
+    // (mkTools drbd [ "drbdadm" "drbdsetup" ])
+    // (mkTools gnugrep [ "egrep" "grep" ])
+    // (mkTools iproute2 [ "bridge" "ip" "tc" ])
+    // (mkTools iptables [ "arptables" "ip6tables" "iptables" ])
+    // (mkTools kmod [ "modinfo" "modprobe" "rmmod" ])
+    // (mkTools libnl [ "nl-qdisc-add" "nl-qdisc-delete" "nl-qdisc-list" ])
+    // (mkTools util-linux [ "flock" "logger" "losetup" "prlimit" ])
+    // {
+      awk = getExe' gawk "awk";
+      brctl = getExe bridge-utils;
+      diff = getExe' diffutils "diff";
+      ifconfig = getExe' inetutils "ifconfig";
+      iscsiadm = getExe' openiscsi "iscsiadm";
+      killall = getExe' psmisc "killall";
+      multipath = getExe' multipath-tools "multipath";
+      nbd-client = getExe' nbd "nbd-client";
+      ovs-vsctl = getExe' openvswitch "ovs-vsctl";
+      sed = getExe gnused;
+      systemd-notify = getExe' systemd "systemd-notify";
+      which = getExe which;
+    };
 
   qubesPatches = import ./patches.nix {
     inherit fetchpatch version revision;
@@ -45,7 +106,7 @@ in
     # Qubes patches first, then nixpkgs build-infra patches (copied locally) to avoid conflicts
     patches = qubesPatchList ++ [
       ./0001-makefile-efi-output-directory.patch
-      ./0002-scripts-external-executable-calls.patch
+      (replaceVars ./0002-scripts-external-executable-calls.patch scriptDeps)
     ];
 
     src = fetchgit {
