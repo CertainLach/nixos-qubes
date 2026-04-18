7ac771e9b4217e8a9d2bc8834b066fc5640ce472
Yaroslav Bolyukin <iam@lach.pw>
2026-04-18 18:19:17 +0200

    build: nix-update no longer downgrades deps

--- update.sh
+++ update.sh
@@ -21,13 +21,9 @@ update "qubes-seabios"
 update "qubes-app-linux-usb-proxy"
 update "qubes-core-admin"
 update "qubes-core-admin-client"
-# update "qubes-core-qrexec"
+update "qubes-core-qrexec"
 update "qubes-desktop-linux-common"
 update "qubes-desktop-linux-manager"
-
-# Nix-update downgrades those D:
-# https://github.com/Mic92/nix-update/issues/271
-nix-update -F "qubes-core-qrexec" -vr "v(.*)" --version v4.3.10
 
 # Needs special care:
 # qubes-core-libvirt - patches nixpkgs libvirt
